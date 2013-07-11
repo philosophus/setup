1aecf024735e2bb004b02ff651f95cde91e18e70
Florian Dahms <florian.dahms@googlemail.com>
2013-07-11 22:10:13 +0000

    Seperate setup script
    Now has a apt-get part that needs root access
    and a node.js installer.

---
+++ basic_apt-get.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# apt-get calls to install basic packages
+# needs root access
+
+# Install Git
+sudo apt-get install -y git-core
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
+# libreadline features
+sudo apt-get install -y rlwrap
---
+++ install_node.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
